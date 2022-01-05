c4cae67180e5fca899fcad018f78bfab5aa8dded
nikox93 <nikox_93@mail.ru>
2022-01-05 11:18:54 +0500

    My first JS code

---
+++ shadowtest/script.js
@@ -0,0 +1,24 @@
+function appear1(){
+document.getElementById("main1").style.boxShadow = 'inset 0px 0px 20px 10px rgba(0,0,0,0.5)';
+}
+function disappear1(){
+document.getElementById("main1").style.boxShadow = 'inset 0px 0px 5px 2px rgba(0,0,0,0.5)';
+}
+function appear2(){
+document.getElementById("main2").style.boxShadow = 'inset 0px 0px 20px 10px rgba(0,0,0,0.5)';
+}
+function disappear2(){
+document.getElementById("main2").style.boxShadow = 'inset 0px 0px 5px 2px rgba(0,0,0,0.5)';
+}
+function appear3(){
+document.getElementById("main3").style.boxShadow = 'inset 0px 0px 20px 10px rgba(0,0,0,0.5)';
+}
+function disappear3(){
+document.getElementById("main3").style.boxShadow = 'inset 0px 0px 5px 2px rgba(0,0,0,0.5)';
+}
+function appear4(){
+document.getElementById("main4").style.boxShadow = 'inset 0px 0px 20px 10px rgba(0,0,0,0.5)';
+}
+function disappear4(){
+document.getElementById("main4").style.boxShadow = 'inset 0px 0px 5px 2px rgba(0,0,0,0.5)';
+}
